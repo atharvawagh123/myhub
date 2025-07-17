4786df9e4b410ef165170adf53115ad1ce1a5b91
atharva <watharva383@gmail.com>
2025-07-17 19:46:10 +0530

    routing set

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,31 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./ProtectRouter/PrivateRoute";
-
+import Feed from "./pages/Feed";
+import Profile from "./pages/Profile";
+import  Search  from "./pages/Search";
 
 function App() {
   return (
     <>
       <ToastContainer position="top-center" autoClose={3000} />
+
       <Routes>
-        <Route path="/" element={
-          <PrivateRoute>
-            <Wrapper />
-          </PrivateRoute>
-        } />
+        {/* Private Routes */}
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Wrapper />
+            </PrivateRoute>
+          }
+        >
+          <Route index element={<Feed />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
